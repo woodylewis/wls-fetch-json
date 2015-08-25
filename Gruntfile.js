79c3afc33c00925964c6eb97031e68e7d63f80fc
woodylewis <woody@woodylewis.com>
2015-08-25 13:11:49 -0400

    Add less and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: false,
+                    cleancss: false,
+                    optimization: 2,
+                    dumpLineNumbers: 'false'
+                },
+                files: {
+                    "app/css/skeleton.css": "app/less/skeleton.less",
+                    "app/css/wls.css": "app/less/wls.less"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['app/less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less', 'watch']);
+};
